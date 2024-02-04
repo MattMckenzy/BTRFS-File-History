324a7189007be8c563a08a241288adc921ec7be2
MattMckenzy <mattmckenzy@ctfam.ca>
2024-02-03 22:58:32 -0500

    Working on new publish script.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
-vsce package
-vsce publish
+CURRENT_VERS=$(cat package.json | jq '.version' -r)
+echo "The Current version is $CURRENT_VERS, enter new version."
+read -p "Version: " -i $CURRENT_VERS -e CHOSEN_VERS
+CHOSEN_VERS=${CHOSEN_VERS:-$CURRENT_VERS}
+echo "Will publish as version: $CHOSEN_VERS"
+jaq -i '.version="'$CHOSEN_VERS'"' package.json
+
+echo "Please enter a changelog and commit message. An empty message will only change version number and not add a changelog entry."
+read -p "Message: " MESSAGE
+
+if [[ -z "$MESSAGE" ]]; then
+   echo "Empty message, will update in place."
+   sed -i 's|## '$CURRENT_VERS'|## '$CHOSEN_VERS'|' CHANGELOG.md
+   sed -i 's|### '$CURRENT_VERS'|### '$CHOSEN_VERS'|' README.md
+   COMMIT_MESSAGE="Bumped to version ${CHOSEN_VERS}"
+else
+   echo "Message, will add changelog entry."
+   set -o noglob
+   set +o histexpand
+   sed -i ''$(grep -Fn '## '$CURRENT_VERS'' CHANGELOG.md | cut -d ":" -f 1)'i## '$CHOSEN_VERS'\n\n- '"$MESSAGE"'\n' CHANGELOG.md
+   sed -i ''$(grep -Fn '### '$CURRENT_VERS'' README.md | cut -d ":" -f 1)'i### '$CHOSEN_VERS'\n\n- '"$MESSAGE"'\n' README.md
+   set +o noglob
+   set -o histexpand
+   COMMIT_MESSAGE="$MESSAGE; bumped to version ${CHOSEN_VERS}"
+fi
+
+mkdir -p packages
+vsce package --out packages/
+#vsce publish
+
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git tag v$CHOSEN_VERS HEAD
+git push --tags
